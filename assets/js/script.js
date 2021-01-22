0d2a8d7b1bfb6d476a0e1b0b77e630abf336e134
Justin Welch <justin.welch075@gmail.com>
2021-01-22 16:00:55 -0600

    added on click event listener for save button

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,18 @@
 var todayDate = moment().format("dddd, MMM Do YYYY");
 
 $("#currentDay").html(todayDate);
-//create on click textarea editing
+// save text data from textarea on click
+$(".saveBtn").on("click", function()
+    {
+        //get values of text and task timeframe
+        var taskEl = $(this).siblings(".description").val();
+        //console.log(taskEl);
+        var timeEl = $(this).parent().attr("id")
+        //console.log(timeEl);
+
+        //save in localStorage
+        localStorage.setItem(taskEl, timeEl);
+    });
 
 //use saveBtn to save tasks to local storage and complete task edit
 
