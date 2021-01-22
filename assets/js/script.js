10b5640a37b0b290ad7306b6405dec8922759e92
Justin Welch <justin.welch075@gmail.com>
2021-01-22 15:13:25 -0600

    current date is displaying and all time blocks in place

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+//display current day of the week and date 
+var todayDate = moment().format("dddd, MMM Do YYYY");
+
+$("#currentDay").html(todayDate);
+//create on click textarea editing
+
+//use saveBtn to save tasks to local storage and complete task edit
+
+//change task background colors to indicate if that time frame is in the past present or future 
