7b343d08e108eeb435cf89f82c26f0dd0acf2fb6
Justin Welch <justin.welch075@gmail.com>
2021-01-22 16:52:31 -0600

    added function for tracking time and changing the class for tasks depending on the current hour

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,9 +12,53 @@ $(".saveBtn").on("click", function()
         //console.log(timeEl);
 
         //save in localStorage
-        localStorage.setItem(taskEl, timeEl);
+        localStorage.setItem(timeEl, taskEl);
     });
 
-//use saveBtn to save tasks to local storage and complete task edit
-
 //change task background colors to indicate if that time frame is in the past present or future 
+
+function trackTime()
+    {
+        //get current time in hours
+        var currentHour = moment().hour();
+        console.log(currentHour);
+
+        //loop over time blocks
+        $(".time-block").each(function()
+        {
+            var timeBlock = parseInt($(this).attr("id").split("hour-")[1]);
+
+            //check time and add the bg classes
+            if(timeBlock < currentHour)
+                {
+                    $(this).removeClass("future");
+                    $(this).removeClass("present");
+                    $(this).addClass("past");
+                }
+            else if(timeBlock === currentHour)
+                {
+                    $(this).removeClass("future");
+                    $(this).removeClass("past");
+                    $(this).addClass("present");
+                }
+            else
+                {
+                    $(this).removeClass("present");
+                    $(this).removeClass("past");
+                    $(this).addClass("future");
+                }
+        })
+    }
+
+//get tasks from localStorage if there are any
+$("#hour-9 .description").val(localStorage.getItem("hour-9"));
+$("#hour-10 .description").val(localStorage.getItem("hour-10"));
+$("#hour-11 .description").val(localStorage.getItem("hour-11"));
+$("#hour-12 .description").val(localStorage.getItem("hour-12"));
+$("#hour-13 .description").val(localStorage.getItem("hour-13"));
+$("#hour-14 .description").val(localStorage.getItem("hour-14"));
+$("#hour-15 .description").val(localStorage.getItem("hour-15"));
+$("#hour-16 .description").val(localStorage.getItem("hour-16"));
+$("#hour-17 .description").val(localStorage.getItem("hour-17"));
+
+trackTime();
